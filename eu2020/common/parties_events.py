055d77a29bce43c82e312dc5fde65e3d67a52714
Michal Porteš <michalportes1@gmail.com>
2021-10-12 12:51:09 +0200

    Use random.choice

--- eu2020/common/parties_events.py
+++ eu2020/common/parties_events.py
@@ -33,10 +33,8 @@ class PartiesEvents:
 
     def get_event(self) -> dict:
         filtered = list(filter(self.event_filter, self.events))
-        if (len(filtered) > 0):
-            n = random.randint(0, len(filtered) - 1)
-            ev = filtered[n]
-            return ev
+        if filtered:
+            return random.choice(filtered)
         else:
             return None
 
