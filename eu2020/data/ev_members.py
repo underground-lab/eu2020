626f3fc3f48da42c3d89c8f991db8949710b5125
Jaroslav Beran <jaroslav.beran@gmail.com>
2021-10-21 10:16:24 +0200

    New event

--- eu2020/data/ev_members.py
+++ eu2020/data/ev_members.py
@@ -347,4 +347,37 @@ ev_member_country = [
             },
         ]
     },
+    {
+        "party": "PL",
+        "description": "Polsko udělalo krok k odchodu z Evropské unie. Ústavní soud rozhodl, že polské právo "
+                       "je nadřazené unijnímu. Postavení ústav a jejich ustanoveních jsou předmětem vášnivé debaty "
+                       "v právnické komunitě v celé Evropské unii.",
+        "options": [
+            {
+                "key": "k",
+                "description": "udělit pokutu 200 000 000 EUR a zahájit kroky k vyloučení Polska z EU",
+                "delay": 12,
+                "impact": {
+                    "satisfaction": {"PL": -5},
+                    "budget": 200_000_000,
+                }
+            },
+            {
+                "key": "p",
+                "description": "přijmout deklaraci proti Polsku a udělit pokutu  200 000 000 EUR",
+                "delay": 12,
+                "impact": {
+                    "satisfaction": {"PL": -5},
+                    "budget": 200_000_000,
+                }
+            },
+            {
+                "key": "n",
+                "description": "nerozhodnout teď",
+                "delay": 1,
+                "impact": {
+                }
+            },
+        ]
+    },
 ]
