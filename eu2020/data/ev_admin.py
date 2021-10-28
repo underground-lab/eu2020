b7e32a8ceb80bbaad229c0a09aaf88c3ab5044d1
Jaroslav Beran <jaroslav.beran@gmail.com>
2021-10-29 00:34:32 +0200

    European Court of Justice

--- eu2020/data/ev_admin.py
+++ eu2020/data/ev_admin.py
@@ -1,9 +1,11 @@
 # coding: utf-8
 
 from eu2020.data.ev_admin_ek import ev_admin_ek
+from eu2020.data.ev_admin_es import ev_admin_es
 
 ev_admin = []
 ev_admin += ev_admin_ek
+ev_admin += ev_admin_es
 
 eu_administration = {
     "EK": {
@@ -24,11 +26,17 @@ eu_administration = {
         "budget_contribution": 0,
         "budget_consumption": 10_000_000_000,
     },
+    "ES": {
+        "name": "Evropský soudní dvůr",
+        "satisfaction_pct": 50,
+        "budget_contribution": 0,
+        "budget_consumption": 2_500_000_000,
+    },
     "AD": {
         "name": "Administrativa",
         "satisfaction_pct": 50,
         "budget_contribution": 0,
-        "budget_consumption": 5_000_000_000,
+        "budget_consumption": 2_500_000_000,
     },
     "PR": {
         "name": "EU Propagace",
---
+++ eu2020/data/ev_admin_es.py
@@ -0,0 +1,42 @@
+# coding: utf-8
+
+ev_admin_es = [
+    {
+        "party": "ES",
+        "description": "Lichtenštejnsko se obrátito na Evropský soud pro lidská práva ve věci "
+                       "navrácení zabaveného majetku. Po válce byly totiž na majetek Lichtenštejnů "
+                       "uplatněny dekrety prezidenta Beneše. Knížecí rodina tvrdí, že jí český stát "
+                       "zabavil majetek nezákonně, protože vládnoucí kníže se za Němce neprohlásil "
+                       "a byl hlavou suverénního státu, který za války zachovával neutralitu.",
+        "options": [
+            {
+                "key": "p",
+                "description": "podpořit Lichtenštejnsko u Evropského soudu",
+                "delay": -1,
+                "impact": {
+                    "satisfaction": {
+                        "CZ": -5
+                    },
+                }
+            },
+            {
+                "key": "e",
+                "description": "nepodpořit Lichtenštejnsko u Evropského soudu",
+                "delay": -1,
+                "impact": {
+                    "satisfaction": {
+                        "CZ": 2
+                    },
+                }
+            },
+            {
+                "key": "n",
+                "description": "nerozhodnout teď",
+                "delay": 2,
+                "impact": {
+                    "satisfaction": {},
+                }
+            },
+        ]
+    }
+]
