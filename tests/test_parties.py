f498083148c37b1c28c8eb68dce84b471ff51e96
Michal Porteš <michalportes1@gmail.com>
2021-11-18 10:31:25 +0100

    Add tests for Parties

--- tests/test_parties.py
+++ tests/test_parties.py
@@ -3,21 +3,74 @@ import pytest
 from eu2020.common.parties import Parties
 
 
-@pytest.mark.parametrize(
-    'parties_data, expected',
-    (
-        ({"X": {"satisfaction_pct": 68}}, 68),
-        (
-            {
-                "A": {"satisfaction_pct": 46},
-                "B": {"satisfaction_pct": 57},
-                "C": {"satisfaction_pct": 67},
-            },
-            56.67
-        ),
+@pytest.fixture
+def parties():
+    data = {
+        "EP": {
+            "name": "Evropský parlament",
+            "satisfaction_pct": 48,
+            "budget_contribution": 0,
+            "budget_consumption": 9_900_000_000,
+        },
+        "CZ": {
+            "name": "Česko",
+            "satisfaction_pct": 64,
+            "budget_contribution": 2_345_000_001,
+            "budget_consumption": 3_456_000_001,
+        },
+        "DK": {
+            "name": "Dánsko",
+            "satisfaction_pct": 57,
+            "budget_contribution": 2_001_000_000,
+            "budget_consumption": 888_000_099,
+        },
+        "IE": {
+            "name": "Irsko",
+            "satisfaction_pct": 72,
+            "budget_contribution": 2_222_000_001,
+            "budget_consumption": 1_555_000_001,
+        },
+        "CN": {
+            "name": "Čína",
+            "satisfaction_pct": 67,
+            "budget_contribution": 0,
+            "budget_consumption": 0,
+        },
+    }
+
+    return Parties(data)
+
+
+def test_get_satisfaction_pct(parties):
+    result = parties.get_satisfaction_pct()
+    assert result == pytest.approx((48 + 64 + 57 + 72 + 67) / 5)
+
+
+def test_get_detailed_satisfaction_report(parties):
+    result = parties.get_detailed_satisfaction_report()
+    assert result == (
+        "Evropský parlament   [magenta]48.00 %[/magenta]\n"
+        "Česko                [magenta]64.00 %[/magenta]\n"
+        "Dánsko               [magenta]57.00 %[/magenta]\n"
+        "Irsko                [magenta]72.00 %[/magenta]\n"
+        "Čína                 [magenta]67.00 %[/magenta]\n"
     )
-)
-def test_average_satisfaction(parties_data, expected):
-    parties = Parties(parties_data)
-    average = parties.get_satisfaction_pct()
-    assert round(average, 2) == expected
+
+
+def test_update_satisfaction(parties):
+    parties.update_satisfaction({"CZ": -2, "DK": -3, "CN": -70, "IE": 2})
+    assert parties.parties["EP"]["satisfaction_pct"] == 48
+    assert parties.parties["CZ"]["satisfaction_pct"] == 62
+    assert parties.parties["DK"]["satisfaction_pct"] == 54
+    assert parties.parties["IE"]["satisfaction_pct"] == 74
+    assert parties.parties["CN"]["satisfaction_pct"] == 0
+
+
+def test_get_budget_contribution(parties):
+    result = parties.get_budget_contribution()
+    assert result == 2_345_000_001 + 2_001_000_000 + 2_222_000_001
+
+
+def test_get_budget_consumption(parties):
+    result = parties.get_budget_consumption()
+    assert result == 9_900_000_000 + 3_456_000_001 + 888_000_099 + 1_555_000_001
