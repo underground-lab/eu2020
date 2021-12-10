fbb0a57b31fae3877c7c439e3817bb0a76c1ed24
Jaroslav Beran <jaroslav.beran@gmail.com>
2021-12-10 23:07:08 +0100

    Terminal size

--- eu2020/__init__.py
+++ eu2020/__init__.py
@@ -1,10 +1,11 @@
 import logging
 import re
+import os
 from rich.console import Console
 from rich.theme import Theme
 
 
-WIDTH = 80
+(WIDTH, HEIGHT) = os.get_terminal_size()
 LOGFILE = 'eu2020.log'
 WRITE_LOG = True
 
