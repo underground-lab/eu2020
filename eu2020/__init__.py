ede58d94bef2ce49161ca7be4dd0dbae8fda6b8b
Jaroslav Beran <jaroslav.beran@gmail.com>
2021-12-10 23:23:36 +0100

    Fix of os.get_terminal_size() exception in pytest

--- eu2020/__init__.py
+++ eu2020/__init__.py
@@ -5,7 +5,8 @@ from rich.console import Console
 from rich.theme import Theme
 
 
-(WIDTH, HEIGHT) = os.get_terminal_size()
+WIDTH = 80
+HEIGHT = 20
 LOGFILE = 'eu2020.log'
 WRITE_LOG = True
 
@@ -17,6 +18,11 @@ def print_log(text, print=True, log=True):
         t = re.sub(r'\[[^\[]*\]', '', text)
         logging.info(t)
 
+
+try:
+    (WIDTH, HEIGHT) = os.get_terminal_size()
+except Exception:
+    print("get_terminal_size error")
 
 logging.basicConfig(filename=LOGFILE, filemode='w', level=logging.DEBUG,
                     format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
