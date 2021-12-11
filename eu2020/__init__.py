ae79344082ac50c7bcdc31736b1551c79dc409a4
Michal Porteš <michalportes1@gmail.com>
2021-12-11 10:49:58 +0100

    Use shutil.get_terminal_size

--- eu2020/__init__.py
+++ eu2020/__init__.py
@@ -1,12 +1,11 @@
 import logging
 import re
-import os
+import shutil
+
 from rich.console import Console
 from rich.theme import Theme
 
-
-WIDTH = 80
-HEIGHT = 20
+WIDTH, HEIGHT = shutil.get_terminal_size()
 LOGFILE = 'eu2020.log'
 WRITE_LOG = True
 
@@ -19,12 +18,8 @@ def print_log(text, print=True, log=True):
         logging.info(t)
 
 
-# pytest doesn't like os.get_terminal_size()
-try:
-    (WIDTH, HEIGHT) = os.get_terminal_size()
-except Exception:
-    print("get_terminal_size error")
-
 logging.basicConfig(filename=LOGFILE, filemode='w', level=logging.DEBUG,
                     format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
 console = Console(theme=Theme(inherit=False), width=WIDTH)
+
+print_log(f"terminal size: {WIDTH} x {HEIGHT}", print=False)
