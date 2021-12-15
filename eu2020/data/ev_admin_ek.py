5a2a320f3f9e1030a978ba70dcaefeb402bf0a5e
Jaroslav Beran <jaroslav.beran@gmail.com>
2021-12-15 23:56:43 +0100

    End of gas heating

--- eu2020/data/ev_admin_ek.py
+++ eu2020/data/ev_admin_ek.py
@@ -188,4 +188,44 @@ ev_admin_ek = [
             },
         ]
     },
+    {
+        "party": "EK",
+        "description": "EK navrhuje do roku 2040 postupně ukončit vytápění budov plynem a dalšími fosilními palivy. "
+                       "Přísnější ekologická pravidla mají platit i pro výstavbu nových budov, které navíc nebudou moci "
+                       "od roku 2030 produkovat žádné emise.",
+        "condition": {
+            "flag": ["green_deal"],
+        },
+        "options": [
+            {
+                "key": "p",
+                "description": "přijmout návrh na postupné ukončení vytápění plynem",
+                "delay": -1,
+                "impact": {
+                    "satisfaction": {
+                        "WHO": 2, "WEF": 2, "SOR": 2, "BE": -2, "BG": -2, "CZ": -2, "DK": -2, "RU": -4,
+                        "EE": -2, "FI": -1, "FR": -2, "HR": -1, "IE": -1, "IT": -1, "CY": -2, "LT": -2, "ES": -2, "SE": -1,
+                        "HU": -2, "MT": -1, "DE": -1, "NL": -1, "PL": -2, "PT": -1, "AT": -1, "RO": -2, "GR": -1, "SK": -2,
+                        "SI": -1, "LV": -1, "LU": -1,
+                    },
+                }
+            },
+            {
+                "key": "o",
+                "description": "odmítnout návrh na postupné ukončení vytápění plynem",
+                "delay": 12,
+                "impact": {
+                    "satisfaction": {"WHO": -1, "WEF": -1, "SOR": -1, "RU": 2},
+                }
+            },
+            {
+                "key": "n",
+                "description": "nerozhodnout teď",
+                "delay": 2,
+                "impact": {
+                    "satisfaction": {},
+                }
+            },
+        ]
+    },
 ]
